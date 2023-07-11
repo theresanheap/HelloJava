a003ab219d06366ca50684b39e4784951c448610
Theresa Nheap <theresa.nheap@gmail.com>
2023-07-11 14:53:39 -0700

    Create JFrame

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        JFrame frame = new JFrame("Hello Java");
+        frame.setSize(300,300);
+        frame.setVisible(true);
     }
 }
